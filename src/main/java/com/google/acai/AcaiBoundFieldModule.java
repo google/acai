8e3e1c133c0c23a3b172364acf0db5885c2898a2
icmdaf <hapa@google.com>
2021-02-11 21:01:14 +0000

    Add AcaiBoundFieldModule (#33)
    * Remove ErrorProne & Javadoc generation
    
    Tried `mvn package`, which failed with a NPE in the compiler, tried to
    bump the maven-compiler-plugins version (same result), configure
    ErrorProne using the new way documented on
    https://errorprone.info/docs/installation, which doesn't crash the
    compiler but produces an error that com.google.auto.AutoValue couldn't
    be found (tried various versions of the AutoValue package, same result).
    I think this is somehow caused by ErrorProne interacting oddly with
    JDK11 and the AutoValue processor.
    
    Javadoc complains about JAVA_HOME not being set. I'm assuming this is
    also a JDK11 issue.
    
    ```
    $ mvn --version
    Apache Maven 3.6.3
    Maven home: /usr/share/maven
    Java version: 11.0.9.1, vendor: Google Inc., runtime:
    /usr/local/buildtools/java/jdk11
    Default locale: en_US, platform encoding: UTF-8
    OS name: "linux",...
    ```
    
    * Add AcaiBoundFieldModule
    
    A wrapper around Guice's BoundFieldModule which resets using an Acai TestingService.
    
    * Add OpenJDK 11 to Travis config
    
    see
    https://docs.travis-ci.com/user/languages/java/#testing-against-multiple-jdks

---
+++ src/main/java/com/google/acai/AcaiBoundFieldModule.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2021 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.acai;
+
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.AbstractModule;
+import com.google.inject.spi.Message;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.google.inject.testing.fieldbinder.BoundFieldModule.BoundFieldInfo;
+import java.util.function.Function;
+
+/**
+ * Wrapper around {@link BoundFieldModule}. Binds all fields annotated with {@code Bind(lazy=true)}
+ * of the given class to a {@code TestScoped} instance, so the values are reset to default for every
+ * test.
+ *
+ * <p>To use the lazy binding is important to either {@code @Inject} a {@code Provider} in your
+ * class under test, or inject a {@code Provider} of the class in your test as shown below.
+ *
+ * <p>Usage:
+ *
+ * <pre>{@code
+ * @Rule public final Acai acai = new Acai(TestEnv.class);
+ *
+ * public static class TestEnv extends AbstractModule {
+ *   protected void configure() { install(AcaiBoundFieldModule.of(Vars.class); }
+ * }
+ *
+ * public static class Vars {
+ *   @Bind(lazy = true) @MyAnnotation String value = "default";
+ * }
+ *
+ * @Inject Vars vars;
+ * @Inject Provider<ClassUnderTest> instance; // injects @MyAnnotation String
+ *
+ * @Test public void test1() {
+ *   vars.value = "test";
+ *
+ *   instance.get().foo(); // uses "test"
+ * }
+ *
+ * @Test public void test2() {
+ *   instance.get().foo(); // uses "default"
+ * }
+ * }</pre>
+ */
+public final class AcaiBoundFieldModule<T> extends AbstractModule {
+  private final Class<T> clazz;
+  private final T instance;
+
+  private AcaiBoundFieldModule(Class<T> clazz) {
+    this.clazz = clazz;
+
+    try {
+      instance = clazz.getDeclaredConstructor().newInstance();
+    } catch (ReflectiveOperationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Use like: {@code install(AcaiBoundFieldModule.of(Vars.class));} in your TestEnv.
+   *
+   * <p>NOTE: The class passed must be {@code public static}.
+   */
+  public static <T> AcaiBoundFieldModule<T> of(Class<T> clazz) {
+    return new AcaiBoundFieldModule<>(clazz);
+  }
+
+  @Override
+  protected void configure() {
+    BoundFieldModule module = BoundFieldModule.of(instance);
+    ImmutableMap<BoundFieldInfo, Object> defaultValues =
+        module.getBoundFields().stream()
+            .filter(
+                info -> {
+                  if (!info.getBindAnnotation().lazy()) {
+                    binder()
+                        .addError(
+                            new Message(
+                                info.getField(), "All bindings must use @Bind(lazy = true)"));
+                    return false;
+                  } else {
+                    return true;
+                  }
+                })
+            .collect(toImmutableMap(Function.identity(), BoundFieldInfo::getValue));
+
+    TestingService testingService =
+        new TestingService() {
+          @BeforeTest
+          void resetInstance() {
+            defaultValues.forEach(
+                (info, value) -> {
+                  try {
+                    info.getField().set(instance, value);
+                  } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                  }
+                });
+          }
+        };
+
+    bind(clazz).toInstance(instance);
+    install(module);
+
+    install(
+        new TestingServiceModule() {
+          @Override
+          protected void configureTestingServices() {
+            bindTestingService(testingService);
+          }
+        });
+  }
+}
